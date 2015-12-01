f0530c87438a2f01ddd2bdd7fa06866c91e09a30
Karuna <imkaruna@gmail.com>
2015-12-01 17:09:58 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_find(collection)
-
+  i=0
+  block_array=[]
+  length = collection.length
+  while length > 0
+   if yield(collection[i])
+     return collection[i]
+   end
+    i+=1
+    length-=1
+    
+  end
+  
 end
