6ed0189fc5fe9fe2602c3e3d963cff620aa54017
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-01-19 13:39:17 -0500

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  array = []
+ 
   while i < collection.length
-    binding.pry
+    
     return collection[i] if yield(collection[i])
     i += 1
   end
