3e8e3bad91e4661fb4d2c2af6e08ec0bb484dd8b
CodeByLine <yleventhal@ymail.com>
2016-02-18 02:52:44 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,10 +3,11 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-   yield(collection[i])
+     return collection[i] if yield(collection[i])
 #For each element in the collection, you're going to yield to the block.
      i += 1
   end
+
 end
 
 
