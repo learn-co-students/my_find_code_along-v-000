4dca526d9f632017f74a54c219775d58687176f6
nOraKat147 <n.mori.iphone@gmail.com>
2016-04-05 04:34:50 -0400

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,12 +5,8 @@ def my_find(collect)
   i = 0
   
   while i < collect.length
-    if yield(collect[i])
-      return collect[i]
+    return collection[i] if yield(collection[i])
     end
     i += 1
   end
 end
-
-collection = (1..100).to_a
-my_find(collection) {|i| i % 3 == 0 && i % 5 == 0 }
