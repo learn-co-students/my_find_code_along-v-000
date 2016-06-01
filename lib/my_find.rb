81a1eaf074c7ec565ea68efd6fbfc8b441460cfc
Michael Stewart <upp_mike@yahoo.com>
2016-05-31 22:03:59 -0700

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
+  counter = 0
+  new_array = []
 
+  while counter < collection.length
+    if yield(collection[counter])
+      return collection[counter]
+    end
+    counter += 1
+  end
 end
