5a329cdcbca1a52a823fb5f78e8f287fe0863741
jack woodward <jackwoodwardschool@gmail.com>
2016-06-06 10:48:43 -0700

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
-
+# this lab shows how the find method in ruby works behind the scenes
 def my_find(collection)
+    i = 0
+    while i < collection.length
+        return collection[i] if yield(collection[i])  #returns the collection element if return is true
+        i += 1
+    end
 
 end
