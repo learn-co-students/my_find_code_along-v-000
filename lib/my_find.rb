64c992c39dc41349b9e91145a44c626afabcfac3
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-02-24 06:49:12 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
-def my_find(collection)
+# Whereas #select will return all elements from the original collection that cause the block to
+# evaluate to true, #find (or #detect) will only return the first element that makes the block true.
 
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
 end
+
+collection = (1..100).to_a
+
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
