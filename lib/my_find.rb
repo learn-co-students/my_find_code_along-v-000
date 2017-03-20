f959d13b181b5b66cb980db89ad73a304b6c5ea3
github username <github email address>
2017-03-20 18:13:07 -0400

    done/raise issue about test wording

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  i=0
+  passing_elements = []
+  while i < collection.length
+    return collection[i] if !!yield(collection[i]) == true
+    i+=1
+  end
+  passing_elements[0]
 end
