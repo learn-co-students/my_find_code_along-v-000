562305c2c6d939d367740e642e58c7cb9376abf4
Yujun Sung <yujunsung@gmail.com>
2017-05-04 21:56:13 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,4 +2,14 @@ require 'pry'
 
 def my_find(collection)
 
+  counter = 0
+  new_collection = []
+
+  while counter < collection.length
+    if yield(collection[counter])
+      return collection[counter]
+    end
+    counter = counter + 1
+  end
+ 
 end
