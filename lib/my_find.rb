91c66f310d3d8f44f0a2b04748b3cea0074bd92e
Yehuda Makarov <yehudamakarov@gmail.com>
2017-07-26 20:22:32 -0400

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
-require 'pry'
 
+collection = (1..100).to_a
 def my_find(collection)
-
+	if collection.empty?
+		puts "The collection is EMPTY!"
+	else
+		i = 0
+		while i < collection.length
+			return collection[i] if yield(collection[i])
+			i += 1
+		end
+	end
 end
+
+my_find(collection) {|i| i % 3 == 0 && i % 5 == 0}
