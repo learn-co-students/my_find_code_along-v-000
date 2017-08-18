f8defef9fb702351ff8cccbc7a01ba36d2c26849
Sofia Candiani <sofia.candiani123@gmail.com>
2017-08-18 22:09:31 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
-require 'pry'
-
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    #If the block returns false,
+    #continue to the next element in the array
+    return collection[i] if yield(collection[i])
+    end
+    i = i + 1
+  end
 end
