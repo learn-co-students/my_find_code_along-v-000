26707fd78cbb75b6429ca19d4ed04fe4b080fede
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-16 20:37:59 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
-require 'pry'
 
 def my_find(collection)
+  x = 0
 
+    while x < collection.length
+      if yield(collection[x])
+        return collection[x]
+      end
+      x += 1
+    end
 end
+
+
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0}
