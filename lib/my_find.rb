091e683b4116050cdc95ae34492650643c304891
Lekan Nicholson <lekan.nicholson@gmail.com>
2017-11-18 11:42:30 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length #low level iterator. We use this and the counter to iterate through collection
+    return collection[i] if yield(collection[i]) #send whatever is passed in as an argument to the block
+    i += 1  #Line 6 will stop the while loop if the yield returns true
+  end
 end
