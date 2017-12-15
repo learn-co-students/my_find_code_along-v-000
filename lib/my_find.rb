f8d745f5a4854e56b07d12cec3c3c870bbab2e7a
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-15 02:29:47 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
-def my_find(collection)
-
+def my_find(col)
+  i = 0
+  while i < col.length
+    return collection[i] if yield(col[i])
+    i += 1
+  end
 end
+
+# Yield Block
+my_find(["0"]) { |i| i % 3 == 0 and i % 5 == 0 }
