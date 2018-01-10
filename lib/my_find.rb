6c11100e121914cc964fcb65904f3a9d2ad07f89
Allyson Wesman <allysonwesman@gmail.com>
2018-01-10 10:23:11 -0800

    implemented my_find

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,4 +2,15 @@ require 'pry'
 
 def my_find(collection)
 
+  i = 0
+  element = ""
+  while i < collection.length
+
+    if yield(collection[i])
+      return collection[i]
+    end
+  
+    i += 1
+  end
+  return nil
 end
