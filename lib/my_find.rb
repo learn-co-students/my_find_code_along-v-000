556a695389c8a8b6a82a7f23a85cb21c49b55cc6
Autumn J <autumnkangas@gmail.com>
2018-01-16 17:11:35 -0600

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+  	#if the return value of this yield is true, return the argument passed in
+  	return collection[i] if yield(collection[i])
+  	i += 1
+  end
 end
