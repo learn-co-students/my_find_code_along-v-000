3127a89d3e977fd84d4cedd4cc34dbc5f231d4ab
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-15 14:42:13 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
+# collection = (1..100).to_a
+# my_find(collection) {|i| i % 3 == 0 && i % 5 == 0 }
+
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
