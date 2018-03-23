5ce56f9e9a027fd086dee8123bdceb5014d7eb8b
Lucy Wall <lhwall@gmail.com>
2018-03-23 15:13:52 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
 def my_find(collection)
-
+i = 0 
+while i < collection.length 
+if yield(collection[i])
+  return collection[i]
+end 
+i += 1 
 end
+end
+
+=begin extremely unlikely anyone will ever see this but I wanted to see if I could find the last matching item in an array instead of the first 
+
+def my_find(collection)
+i = 0 
+
+while i < collection.length 
+if yield(collection[collection.length - i - 1])
+  return collection[collection.length - i - 1]
+end 
+
+i += 1 
+end
+end
+=end
