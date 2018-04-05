bf7e64f3caf705e624e1514bedeac2be005e6b0d
Diogo Ide <diogoide@gmail.com>
2018-04-05 00:12:48 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+      return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
+
+collection = (1..100).to_a
+my_find(collection) {|i| 1 % 3 == 0 and i % 5 == 0}
+
+
+#If at any point you want to follow along with the example, you can put a binding.pry anywhere in the method and run ruby bin/my_find in your terminal to pry around.
