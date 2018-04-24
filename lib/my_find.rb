76307b92c8a4660aa6deaf3db46830a40fa4c25d
Eric Fiorani <efiorani305@gmail.com>
2018-04-24 15:43:40 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
+collection = (1..100).to_a
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
