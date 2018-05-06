8179bbe790998bc1e66e0fb494c82ac044f89143
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-06 01:08:10 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])   #We can refactor this slightly by putting our if statement all on one line using a statement modifier:
+   i = i + 1                                         #if yield(collection[i])
+ end                                                 #return collection[i]
 end
+
+#    def my_find(collection)
+#      i = 0
+#   while i < collection.length
+#    if yield(collection[i])
+#      return collection[i]
+#    end
+#    i = i + 1
+#  end
+# end
