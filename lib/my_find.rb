e199f0a9d4ed8f11d526d238b9662bb38b170cbe
Brandon Green <brandon@brandonleeproductions.com>
2018-05-28 18:01:25 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0 
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i += 1 
+  end
 end
+
+
+#refactored code :
+#
+# def my_find(collection)
+#  i = 0 
+# while i < collection.length
+#      return collection[i] if  #added if to this line to refactor
+#  yield(collection[i])
+#    i += 1 
+#  end
+# end
