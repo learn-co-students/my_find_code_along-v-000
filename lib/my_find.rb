4e1acc5a6d182e49e4fffc350ba3e1f339cd7327
Anusha Konatala <anu.konatala@gmail.com>
2018-06-04 23:33:31 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,10 @@ require 'pry'
 def my_find(collection)
    i = 0
    while i < collection.length
-    if yield(collection[i])
-      return collection[i]
-    end
+   # if yield(collection[i])
+  #    return collection[i]
+  #    end
+  return collection[i] if yield(collection[i])
     i = i + 1
    
     end
