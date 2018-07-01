283cda8d49a117ce0728714fb4e2df779288d702
Ana Bebic <anabebic.x@gmail.com>
2018-07-01 11:09:14 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,12 @@
-require 'pry'
-
 def my_find(collection)
-
-i = 0 
-while i < collection.length 
-
-yield(collection[i])
-  i = i + 1 
-  end   
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
 end
+
+  
+  
+  
+  
