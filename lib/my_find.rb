1c6d407273f4e74cfcb25f71ead7970ed5d041df
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-22 04:53:51 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
+collection = []
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+      return collection[i] if yield(collection[i])
+      i += 1
+  end
 end
+
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0}
