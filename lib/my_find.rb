5076ce8dea864ac177350198e168721615d6ae2e
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 16:41:33 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+  if collection != []
+    i = 0 
+    while i < collection.length 
+      return collection[i] if yield(collection[i])
+      i = i + 1
+    end
+  else
+    "No data in collection"
+  end
 end
