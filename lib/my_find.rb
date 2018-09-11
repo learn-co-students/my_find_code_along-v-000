3f0a41e7b4481702950f2793cdaf616e2cfb2c19
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-11 05:32:48 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i += 0
+  end
 end
+binding.pry
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 && i % 5 == 0}
