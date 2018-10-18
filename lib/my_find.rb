77e254f55f3bbae7a60d63273323c82d51c060a9
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-18 02:17:22 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0
+  finds = []
 
+  while i < collection.size
+    return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
+
+y = []
+coll = (1..100).to_a
+my_find(coll) do |number|
+  y.push number
+end
+#puts my_find([1,2,3]){ |i| i ==3 }
+puts y 
