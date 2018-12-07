30b01d2ca94fcad5a7056f703ef8aaa6d96a14ca
Lauren Days <lmchambers0420@gmail.com>
2018-12-07 03:44:35 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
+#Rememeber to comment code 
 
 def my_find(collection)
-
+  i = 0 
+  while i < collection.length 
+      return collection[i] if
+    yield(collection[i])
+    i = i += 1 #i can put += if its easier to remember
+  end
 end
